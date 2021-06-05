296324681e0ea410d8b46c542d4d77b1d4ff02c6
Decoder6125 <mangapappale123@gmail.com>
2021-06-04 17:46:52 -0700

    fixing login issues

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken')
 const {JWT_SECRET} = require('../keys')
 const requireLogin = require('../middleware/requireLogin')
 
-router.get('/protected', requireLogin ,(req,res) => {
+router.get('/protected',requireLogin,(req,res) => {
     res.send("Hello user")
 })
 
