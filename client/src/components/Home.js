7b3bbf1128b10e4d4222a3bf5fb0c094c6647b8e
Decoder6125 <mangapappale123@gmail.com>
2021-06-25 08:32:12 -0700

    Home ui

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,30 @@
 import React from 'react'
-
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import SendIcon from '@material-ui/icons/Send';
+import './form.css';
 const Home = () => {
     return (
-        <div>
-          <br></br><br></br> <br></br><br></br> <br></br><br></br>  <br></br><br></br>
-           <center><h1>Home/decoder</h1></center> 
-    </div>
+       <>
+       <div className="post_card">
+           <div className="post_name">Debtanu Dey</div>
+           <div className="post_image"><img src="https://i.pinimg.com/originals/d6/8f/71/d68f712248a77ff122bfbad8be5a162d.jpg" height="100%" width="100%"></img></div>
+           <div className="post_likes"><FavoriteBorderIcon /></div>
+           <div className="post_comments">
+           <input className="comment" placeholder="comment" />
+                 <button className="submit" type="submit"><SendIcon/></button>
+           </div>
+       </div>
+
+       <div className="post_card">
+           <div className="post_name">Debtanu Dey</div>
+           <div className="post_image"><img src="https://i.pinimg.com/originals/d6/8f/71/d68f712248a77ff122bfbad8be5a162d.jpg" height="100%" width="100%"></img></div>
+           <div className="post_likes"><FavoriteBorderIcon /></div>
+           <div className="post_comments">
+           <input className="comment" placeholder="comment" />
+                 <button className="submit" type="submit"><SendIcon/></button>
+           </div>
+       </div><br></br>
+       </>
     )
 }
 
